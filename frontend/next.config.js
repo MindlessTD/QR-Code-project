321c65ea9f6157e2af68a1ce1829cfd79576649c
MindlessTD <142829268+MindlessTD@users.noreply.github.com>
2025-01-26 21:35:16 -0600

    Add files via upload

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+module.exports = {
+    output: 'export', // Enables static export
+    trailingSlash: true, // Ensures all paths end with a slash (optional but recommended)
+  };
+  
